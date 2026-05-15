59ed2280cc81cd78d0a8ec14d2d3d1a674b428b9
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2026-05-15 17:32:56 +0200

    Aggiunta sezione contatti

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Projects from './components/Projects/Projects';
+import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import './App.css';
 
@@ -12,6 +13,7 @@ const App = () => {
       <main>
         <Hero />
         <Projects />
+        <Contact />
       </main>
       <Footer />
     </div>

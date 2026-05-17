52acdc7cab1583a3848a055727e34b2763361db6
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2026-05-17 18:32:31 +0200

    revisionati contact e footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,25 +2,33 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  // Otteniamo l'anno corrente dinamicamente per non doverlo aggiornare a mano
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="footer-container">
       <div className="footer-content">
+        
+        {/* Info personali */}
         <div className="footer-info">
           <h3>Luca Ruggeri</h3>
           <p>Junior React Developer con la passione per il design pulito.</p>
         </div>
 
+        {/* Link rapidi reali coordinati con la sezione contatti */}
         <div className="footer-socials">
-          {/* Sostituisci i # con i tuoi link reali quando pronti */}
-          <a href="https://github.com" target="_blank" rel="noreferrer">GitHub</a>
-          <a href="https://linkedin.com" target="_blank" rel="noreferrer">LinkedIn</a>
-          <a href="mailto:tuaemail@esempio.com">Email</a>
+          <a href="https://github.com/LucaRuggeri5" target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+          <a href="https://www.linkedin.com/in/luca-ruggeri97/" target="_blank" rel="noreferrer">
+            LinkedIn
+          </a>
+          <a href="mailto:luca.ruggeri5@gmail.com">
+            Email
+          </a>
         </div>
       </div>
 
+      {/* Copyright */}
       <div className="footer-bottom">
         <p>&copy; {currentYear} - Creato con React & Vite</p>
       </div>
